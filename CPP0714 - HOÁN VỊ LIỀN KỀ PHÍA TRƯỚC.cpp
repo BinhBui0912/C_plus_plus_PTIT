d019e43cc4698c3bfcaf6a052846edd4468bcaef
BinhBui0912 <duybinhh03@gmail.com>
2022-09-25 01:49:28 +0700

    Source Code

--- CPP0714 - HOÁN VỊ LIỀN KỀ PHÍA TRƯỚC.cpp
+++ CPP0714 - HOÁN VỊ LIỀN KỀ PHÍA TRƯỚC.cpp
@@ -1,30 +1,33 @@
-//cac dieu can ghi nho trong string
-//length() va size()
-//cin.ignore va getline(cin,s)
-//substr(n,k): cat xau
-//stoi: chuyen thanh kieu int
-//stoll: chuyen thanh kieu longlong
-//to_string(): chuyen so thanh string
-//stringstream ss(): tach tu theo dau cach
-
+////cac dieu can ghi nho trong string
+////length() va size()
+////cin.ignore va getline(cin,s)
+////substr(n,k): cat xau
+////stoi: chuyen thanh kieu int
+////stoll: chuyen thanh kieu longlong
+////to_string(): chuyen so thanh string
+////stringstream ss(): tach tu theo dau cach
+////s.insert(k, value): chen them gia tri value sau vi tri k tai xau s
+//
 #include<bits/stdc++.h>
 
 using namespace std;
 
-int n, i, a[10000];
+int n, k, check, a[10000];
+
 
 void sinh(){
-	int i = n - 1;
-	while( i >= 1 && a[i] < a[i+1]){
-		i--;
+	int i = n-1;
+	while(i >= 1 && a[i] < a[i+1]){
+		--i;
 	}
-	swap(a[i], a[i+1]);
-	reverse(a+i, a+n);
+	int j = n;
+	while(a[i] < a[j]) j--;
+	swap(a[i], a[j]);
+	reverse(a+i+1, a+n+1);
 }
 
 int main(){
-	int t;
-	cin >> t;
+	int t; cin >> t;
 	while(t--){
 		cin >> n;
 		for(int i = 1; i <= n; i++){
@@ -37,3 +40,4 @@ int main(){
 		cout << endl;
 	}
 }
+
