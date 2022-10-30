776d03c7c98143b289a8368d6fe99685c17eb3f0
BinhBui0912 <114017752+BinhBui0912@users.noreply.github.com>
2022-10-30 11:16:43 +0700

    Add files via upload
    Source code

---
+++ CPP0801 - SAO CHÉP TỆP TIN.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h> 
+
+using namespace std;
+
+int main(){
+	freopen("PTIT.in", "r", stdin);
+	freopen("PTIT.out", "w", stdout);
+	string s;
+	while(getline(cin,s)){
+		cout << s << endl;
+	}
+}
+
+
